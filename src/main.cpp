2e66f717e7b6681473ad7b70b9c1c252f5607704
akshay1992kalbhor <akshay1992kalbhor@gmail.com>
2020-06-20 00:41:31 +0530

    Add a basic barebone implementation of Memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,33 @@
-int main() {
+#include <cstdint>
+#include <vector>
+#include <stdexcept>
 
-	return 0;
+struct Memory {
+public:
+  Memory() : main(capacity, 0) {}
+  ~Memory() {}
+  bool set_value(size_t location, uint8_t value) {
+	if (location < capacity) {
+		main[location] = value;
+		return true;
+	} else {
+		return false;
+	}
+  }
+  uint8_t get_value(size_t location) {
+	if (location < capacity) {
+		return main[location];
+	} else {
+		throw std::out_of_range("Invalid memory access"); 
+	}
+  }
+
+private:
+  constexpr static size_t capacity = 4096;
+  std::vector<uint8_t> main;
+};
+
+int main() {
+  Memory mem{};
+  return 0;
 }
