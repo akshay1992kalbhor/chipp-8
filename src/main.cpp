d7e7b8838b2ac3f7e7bc047bf480e4e5e7e39b47
akshay1992kalbhor <akshay1992kalbhor@gmail.com>
2020-08-13 23:41:22 +0530

    Implemented the code for an instruction: 0xFX55

--- src/main.cpp
+++ src/main.cpp
@@ -29,18 +29,21 @@ struct CPU {
         uint16_t tcalled = 0;
 		std::vector<uint16_t> is{};
         
-		while (iaddress != 0x23C ) {
+		while (iaddress != 0x23C) {
             
 			uint8_t fb = ram.main[iaddress];
             uint8_t sb = ram.main[iaddress + 1];
-            
-			if (iaddress == 0x214) {
-                tcalled++;
-            }
+          	/* 
+		   	switch (fb >> 4) {
+				case 0x0:
+					switch (sb) {
+						case 0xE0:
+							display.clear_screen();
+					}
+			}*/	
 			
 			if (fb >> 4 == 0x0) {
                 if (fb != 0) {
-                    std::cout << "CALL ML SUBROUTINE\n";
 					throw "Unimplemented!";
                 } else {
                     if (sb == 0xE0) {
@@ -48,121 +51,95 @@ struct CPU {
                     } else if (sb == 0xEE) {
                         iaddress = stack.back();
                         stack.pop_back();
-                        std::cout << "RETURN FROM SUBROUTINE\n";
                         continue;
                     } else {
-                        std::cout << "UNRECOGNIZED INSTRUCTION 0x0\n";
+                        throw "Unrecognized Instruction: 0x0XXX";
                     }
                 }
             } 
-			
-			
-			
-			
-			
-			
 			else if (fb >> 4 == 0x1) {
                 uint16_t address = ((fb & 0x0F) << 8) | sb;
-                // stack.push_back(iaddress + 2);
                 iaddress = address;
                 std::cout << "JUMP TO ADDRESS\n";
                 continue;
-            
-			
-			
-			} else if (fb >> 4 == 0x2) {
+			}
+			else if (fb >> 4 == 0x2) {
                 uint16_t one = ((fb & 0x0F) << 8) | sb;
-                std::cout << "ONE: " << std::hex << one << std::endl;
                 uint16_t address = ((fb & 0x0F) << 8) | sb;
                 stack.push_back(iaddress + 2);
                 iaddress = address;
-                std::cout << "CALL SUBROUTINE\n";
                 continue;
-            
-			
-			
-			} else if (fb >> 4 == 0x3) {
+			}
+			else if (fb >> 4 == 0x3) {
                 auto number = sb;
                 uint8_t reg_index = fb & 0x0F;
                 if (rs.registers[reg_index] == number) {
                     iaddress += 2;
                 }
-                std::cout << "SKIP NXT INST IF VX==NN\n";
-            
-			
-			
-			
-			} else if (fb >> 4 == 0x4) {
+			}
+			else if (fb >> 4 == 0x4) {
                 auto number = sb;
                 uint8_t reg_index = fb & 0x0F;
                 if (rs.registers[reg_index] != number) {
                     iaddress += 2;
-                    std::cout << "SKIPPING NXT INST\n";
-                } else {
-                    std::cout << "NOT SKIPPING NXT INST\n";
                 }
-            } else if (fb >> 4 == 0x5) {
+            }
+			else if (fb >> 4 == 0x5) {
                 uint8_t reg_index = sb >> 4;
                 uint8_t reg_index2 = fb & 0x0F;
                 if (rs.registers[reg_index] == rs.registers[reg_index2]) {
                     iaddress += 2;
                 }
-                std::cout << "SKIP NXT INST IF VX == VY\n";
-            } else if (fb >> 4 == 0x6) {
+            }
+			else if (fb >> 4 == 0x6) {
                 uint8_t number = sb;
                 uint8_t reg = fb & 0x0F;
                 rs.store_num_in_register(number, reg);
-                std::cout << "STORE " << +number << " IN V" << +reg << "\n";
-            } else if (fb >> 4 == 0x7) {
+            }
+			else if (fb >> 4 == 0x7) {
                 auto number = sb;
                 auto reg = fb & 0x0F;
                 rs.add_num_in_register(number, reg);
-                std::cout << "ADD NN TO VX\n";
-            } else if (fb >> 4 == 0x8) {
-
+            }
+			else if (fb >> 4 == 0x8) {
                 if ((sb & 0x0F) == 0x0) {
                     uint8_t from = sb >> 4;
                     uint8_t to = fb & 0x0F;
                     rs.store_reg_value_to_register(from, to);
-                    std::cout << "V" << +to << " = V" << +from << "\n";
-                } else if ((sb & 0x0F) == 0x2) {
+                }
+				else if ((sb & 0x0F) == 0x2) {
                     uint8_t reg_one = sb >> 4;
                     uint8_t reg_two = fb & 0x0F;
                     rs.logical_and(reg_one, reg_two);
-                    std::cout << "V" << +reg_two << " &= V" << +reg_one << "\n";
-                } else if ((sb & 0x0F) == 0x4) {
+                }
+				else if ((sb & 0x0F) == 0x4) {
                     uint8_t reg_one = sb >> 4;
                     uint8_t reg_two = fb & 0x0F;
                     rs.add_reg_value_to_register(reg_one, reg_two);
-                    std::cout << "V" << +reg_two << " += V" << +reg_one << "\n";
                 }
-
-            } else if (fb >> 4 == 0x9) {
+            }
+			else if (fb >> 4 == 0x9) {
                 uint8_t reg_index = sb >> 4;
                 uint8_t reg_index2 = fb & 0x0F;
                 if (rs.registers[reg_index] != rs.registers[reg_index2]) {
                     iaddress += 2;
                 }
-                std::cout << "SKIP NXT INST IF VX != VY\n";
-            } else if (fb >> 4 == 0xA) {
-				
+            }
+			else if (fb >> 4 == 0xA) {
                 uint16_t address = ((fb & 0x0F) << 8) | sb;
                 rs.set_I_register(address);
-				std::cout << "XX3 =" << rs.Ireg << std::endl;
 				is.push_back(address);
-                std::cout << "STORE " << address << " IN REG I\n";
-
-            } else if (fb >> 4 == 0xB) {
+            }
+			else if (fb >> 4 == 0xB) {
                 uint16_t address = ((fb & 0x0F) << 2) | sb;
                 rs.jmp_to_address_offset(address);
-                std::cout << "JMP TO ADDRESS NNN + V0\n";
-            } else if (fb >> 4 == 0xC) {
+            }
+			else if (fb >> 4 == 0xC) {
                 auto reg = fb & 0x0F;
                 auto mask = sb;
                 rs.set_reg_to_rand_with_mask(reg, mask);
-                std::cout << "SET VX TO A RAND NUM WITH MASK NN\n";
-            } else if (fb >> 4 == 0xD) {
-
+            }
+			else if (fb >> 4 == 0xD) {
                 uint8_t reg_index2 = sb >> 4;
                 uint8_t reg_index = fb & 0x0F;
                 uint8_t size = sb & 0x0F;
@@ -171,18 +148,9 @@ struct CPU {
                 uint8_t y = rs.registers[reg_index2];
 
                 for (uint8_t i = 0; i < size; i++) {
-
                     uint8_t byte = ram.main[rs.Ireg + i];
-
-                    // std::cout << "SDATA: " << +byte << " BYTES: ";
-
                     std::deque<uint8_t> bvec(8, 0);
-                    // E0 => 1110 0000
-                    // 20 => 0010 0000
-                    // E0 => 1110 0000
-                    // 80 => 1000 0000
-                    // E0 => 1110 0000
-                    // {0000 0111}
+                    
                     int idx = 7;
                     while (byte > 0 || idx >= 0) {
                         if (byte % 2 == 1) {
@@ -193,11 +161,6 @@ struct CPU {
                         byte /= 2;
                         idx--;
                     }
-                    /*for (const auto &e : bvec) {
-                        std::cout << +e;
-                    }
-                    std::cout << std::endl;*/
-
                     for (uint8_t j = 0; j < 8; j++) {
                         if (y + i > 0 && y + i < 32) {
                             if (x + j > 0 && x + j < 64) {
@@ -210,89 +173,82 @@ struct CPU {
                         }
                     }
                 }
-
-                std::cout << "DRW SPRITE AT V" << +reg_index << ",V"
-                          << +reg_index2 << " WITH " << +size
-                          << " BYTES OF SPRITE DATA STORED AT I: " << rs.Ireg
-                          << "\n";
                 display.set_screen(buffer);
-
-            } else if (fb >> 4 == 0xE) {
+            }
+			else if (fb >> 4 == 0xE) {
                 if (sb == 0x9E) {
                     auto reg_index = fb & 0x0F;
-                    std::cout
-                        << "SKIP NXT INSTRUCTION IF KEY PRESSED == VAL IN VX\n";
-                } else if (sb == 0xA1) {
-                    auto reg_index = fb & 0x0F;
-                    std::cout
-                        << "SKIP NXT INSTRUCTION IF KEY PRESSED != VAL IN VX\n";
-                } else {
                 }
-            } else if (fb >> 4 == 0xF) {
+				else if (sb == 0xA1) {
+                    auto reg_index = fb & 0x0F;
+                }
+				else {
+					throw "Unrecognized Instruction: 0xEXXX";
+                }
+            }
+			else if (fb >> 4 == 0xF) {
                 if (sb == 0x07) {
-
                     auto reg_index = fb & 0x0F;
                     rs.registers[reg_index] = rs.read_timer();
-                    std::cout << "STORE CV OF DELAY TIMER IN VX\n";
-                } else if (sb == 0x0A) {
-
+                }
+				else if (sb == 0x0A) {
                     uint8_t reg_index = fb & 0x0F;
-                    std::cout << "UI: WAIT FOR KEYPRESS AND STORE VAL IN VX\n";
 					while (key == Keypress::nil) {
 						// not great
 					}
-					std::cout << "FINALLY A KEPRESS" << std::endl;
                     rs.registers[reg_index] = static_cast<int>(key);
 					key = Keypress::nil;
-                } else if (sb == 0x15) {
-
+                }
+				else if (sb == 0x15) {
                     auto reg_index = fb & 0x0F;
                     rs.set_timer(rs.registers[reg_index]);
-                    std::cout << "SET D.TIMER TO VAL OF REG VX\n";
-                } else if (sb == 0x18) {
+                }
+				else if (sb == 0x18) {
                     auto reg_index = fb & 0x0F;
                     rs.set_sound_timer(rs.registers[reg_index]);
-                    std::cout << "SET S.TIMER TO VAL OF REG VX\n";
-                } else if (sb == 0x1E) {
+                }
+				else if (sb == 0x1E) {
                     auto reg_index = fb & 0x0F;
                     rs.Ireg += rs.registers[reg_index];
-					std::cout << "V3 =" << +rs.registers[reg_index] << std::endl;
-					std::cout << "XX3 =" << rs.Ireg << std::endl;
 					is.push_back(rs.Ireg);
-                    std::cout << "ADD VAL IN VX TO I\n";
-                } else if (sb == 0x29) {
-                    std::cout << "UI: SOMETHING TO DO WITH SPRITES\n";
-                } else if (sb == 0x55) {
-                    std::cout << "UI: WAIT FOR KEYPRESS AND STORE VAL IN VX\n";
-                } else if (sb == 0x33) {
-
+                }
+				else if (sb == 0x29) {
+                    throw "Unimplemented Instruction: 0xFX29";
+                }
+				else if (sb == 0x55) {
+					/* Store the values from registers 'V0' to 'VX' in memory starting from the address
+					 * stored in register 'I'.
+					 */
+					uint8_t max_reg = fb & 0x0F;
+					for (uint8_t i = 0; i <= max_reg; i++) {
+						ram.main[rs.Ireg + i] = rs.registers[i];
+					}
+                }
+				else if (sb == 0x33) {
                     uint8_t reg = fb & 0x0F;
                     uint8_t val = rs.registers[reg];
                     uint8_t add = 0;
                     uint8_t num = 100;
                     while (add <= 2) {
-                        std::cout << "HERE: " << val / num << std::endl;
                         ram.main[rs.Ireg + add] = val / num;
                         val %= num;
                         num /= 10;
                         add++;
                     }
-					//rs.Ireg = rs.Ireg + reg + 1;
-                } else if (sb == 0x65) {
+                }
+				else if (sb == 0x65) {
                     uint8_t max_reg = (fb & 0x0F);
                     for (uint8_t i = 0; i <= max_reg; i++) {
                         rs.registers[i] = ram.main[rs.Ireg + i];
                     }
-					//rs.Ireg = rs.Ireg + max_reg + 1;
-                    std::cout << "8/16 FILL V0-VX WITH VALUES FROM "
-                                 "MEM[I],MEM[I+1],..\n";
                 }
             } else {
-                std::cout << "UNRECOGNIZED INSTRUCTION\n";
+                throw "Unrecognized Instruction";
             }
             iaddress += 2;
         }
-        ram.print_mem(0x294, 3);
+        
+		ram.print_mem(0x294, 3);
         rs.print_registers();
 		for (const auto& e : is) {
 			std::cout << e << " ";
